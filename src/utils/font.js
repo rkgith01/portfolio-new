4610b43edbf29ccf0765125960c46646eaa3812c
rkgith01 <rajkapadia34@gmail.com>
2025-05-28 16:23:15 +0530

    font preload fix

--- src/utils/font.js
+++ src/utils/font.js
@@ -21,38 +21,49 @@ const lobster = Lobster({
   subsets: ["latin"],
   weight: "400",
   variable: "--font-lobster",
+  display: "swap",
 });
 
 // Load fonts with CSS variables
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-poppins",
+  display: "swap",
 });
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-roboto",
+  display: "swap",
 });
 const merriweather = Merriweather({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-merriweather",
+  display: "swap",
 });
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-montserrat",
+  display: "swap",
 });
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export {
